d48519c71da77f7a802293e4c23071e14abf7364
eddy3721 <eddyqwq@gmail.com>
2022-04-09 14:54:02 +0800

    Update skill.js

--- js/skill.js
+++ js/skill.js
@@ -1,8 +1,60 @@
-function sk_1(m) { //水槍
-    let dmg = Math.ceil(m['MATK'] * 1.2);
-    let obj = {
-        "dmg": dmg,
-        "msg": '水槍'
+function sk(n, a, b) {
+    let dmg;
+    let obj;
+    switch (n) {
+        case 1: //水槍
+            dmg = Math.ceil(a['MATK'] * 1.2);
+            dmg = getDamage(dmg, b['DEF'], a['STB']);
+
+            obj = {
+                "dmg": dmg,
+                "msg": '水槍'
+            }
+            return obj;
+        case 2: //衝撞
+            dmg = Math.ceil(a['ATK'] * 1.1);
+            dmg = getDamage(dmg, b['DEF'], a['STB']);
+
+            obj = {
+                "dmg": dmg,
+                "msg": '衝撞'
+            }
+            return obj;
+        case 3: //上頂
+            dmg = Math.ceil(a['ATK'] * 1.3);
+            dmg = getDamage(dmg, b['DEF'], a['STB']);
+
+            obj = {
+                "dmg": dmg,
+                "msg": '上頂'
+            }
+            return obj;
+        case 4: //純淨水槍
+            dmg = Math.ceil(a['MATK'] * 1.4);
+            dmg = getDamage(dmg, b['DEF'], a['STB']);
+
+            obj = {
+                "dmg": dmg,
+                "msg": '純淨水槍'
+            }
+            return obj;
+        case 5: //水砲
+            dmg = Math.ceil(a['MATK'] * 1.5);
+            dmg = getDamage(dmg, b['DEF'], a['STB']);
+
+            obj = {
+                "dmg": dmg,
+                "msg": '水砲'
+            }
+            return obj;
+        case 6: //黃金衝撞
+            dmg = Math.ceil(a['ATK'] * 1.4);
+            dmg = getDamage(dmg, b['DEF'], a['STB']);
+
+            obj = {
+                "dmg": dmg,
+                "msg": '黃金衝撞'
+            }
+            return obj;
     }
-    return obj;
 }
