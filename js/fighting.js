6232fe84e4f24f935406dad691fd8a1c41ef4236
eddy3721 <eddyqwq@gmail.com>
2022-04-09 13:11:07 +0800

    Update fighting.js

--- js/fighting.js
+++ js/fighting.js
@@ -1,11 +1,3 @@
-var monster = [
-    ['史萊姆', 20, 10, 10, 0, 0, 1],
-    ['野豬', 35, 13, 0, 4, 4, 2],
-    ['黃金史萊姆', 45, 15, 15, 5, 5, 5],
-    ['甲福', 50, 17, 17, 6, 6, 7],
-    ['史萊姆王', 120, 20, 20, 9, 9, 10]
-]
-
 //野外戰鬥
 async function fighting(n) {
     $('#fight_1').attr('disabled', true);
@@ -13,38 +5,41 @@ async function fighting(n) {
     let content = '<p class="subtitle" id="report_title">行動報告</p>';
     let CD = [10, 30, 60];
     let user = JSON.parse(decode(localStorage.getItem("userInfo"), key));
-    let m = monster[Math.floor(Math.random() * monster.length)].concat();
+    let place = localStorage.getItem("user_place");
+    let rand = Math.floor(Math.random() * M[place].length);
+    let m = JSON.parse(JSON.stringify(M[place][rand]));
+
     let attacker = 0;
     let i = 2;
     let getEXP = 0;
     let getMoney = 0;
 
     content += '<div class="flex"><div class="numberReportLine">1</div>';
-    content += user['user_name'] + ' 遇到了 ' + m[0] + '(lv.' + m[6] + ') !</div>';
+    content += user['user_name'] + ' 遇到了 ' + m['name'] + '(lv.' + m['LV'] + ') !</div>';
 
-    while (user['user_HP'] > 0 && m[1] > 0) {
+    while (user['user_HP'] > 0 && m['HP'] > 0) {
         if (attacker == 0) {
-            dmg = getDamage(user['user_ATK'], m[4], user['user_STB']);
-            m[1] -= dmg;
+            dmg = getDamage(user['user_ATK'], m['DEF'], user['user_STB']);
+            m['HP'] -= dmg;
             attacker = 1;
             content += '<div class="flex"><div class="numberReportLine">' + i + '</div>';
-            content += user['user_name'] + ' 對 ' + m[0] + ' 造成了' + dmg + '點傷害</div>';
+            content += user['user_name'] + ' 對 ' + m['name'] + ' 造成了' + dmg + '點傷害</div>';
         } else {
-            dmg = getDamage(m[2], user['user_DEF'], 70);
+            dmg = getDamage(m['ATK'], user['user_DEF'], 70);
             user['user_HP'] -= dmg;
             attacker = 0;
             content += '<div class="flex"><div class="numberReportLine">' + i + '</div>';
-            content += m[0] + ' 對 ' + user['user_name'] + ' 造成了' + dmg + '點傷害</div>';
+            content += m['name'] + ' 對 ' + user['user_name'] + ' 造成了' + dmg + '點傷害</div>';
         }
         i++;
     }
 
-    if (m[1] <= 0) {
+    if (m['HP'] <= 0) {
         content += '<div class="flex"><div class="numberReportLine">' + i + '</div>';
-        content += m[0] + ' 倒下了，' + user['user_name'] + ' 還有 ' + user['user_HP'] + ' 點血量</div>';
+        content += m['name'] + ' 倒下了，' + user['user_name'] + ' 還有 ' + user['user_HP'] + ' 點血量</div>';
         i++;
         //經驗值計算
-        getEXP = (10 - (Math.abs(m[6] - user['user_LV']))) * m[6];
+        getEXP = (10 - (Math.abs(m['LV'] - user['user_LV']))) * m['LV'];
         if (getEXP < 0) {
             getEXP = 0;
         }
@@ -53,11 +48,11 @@ async function fighting(n) {
         EXP_update(getEXP);
     } else {
         content += '<div class="flex"><div class="numberReportLine">' + i + '</div>';
-        content += user['user_name'] + ' 戰敗了! ' + m[0] + '還有 ' + m[1] + ' 點血量</div>';
+        content += user['user_name'] + ' 戰敗了! ' + m['name'] + '還有 ' + m['HP'] + ' 點血量</div>';
         i++;
 
         //戰敗懲罰
-        getMoney = (Math.abs(m[6] - user['user_LV'])) * m[6] * -0.2;
+        getMoney = (Math.abs(m['LV'] - user['user_LV'])) * m['LV'] * -0.2;
         content += '<div class="flex"><div class="numberReportLine">' + i + '</div>';
         content += '你損失了 ' + (getMoney * -1) + ' 眾神幣</div>';
         money_update(getMoney);
