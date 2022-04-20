d60f79adc05673f131bd0e1f1353be59afcb9f14
eddy3721 <eddyqwq@gmail.com>
2022-04-20 12:30:21 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -337,7 +337,7 @@ function get_gift() {
         toast: true,
         position: 'top-end',
         showConfirmButton: false,
-        timer: 2500,
+        timer: 1000,
         timerProgressBar: true,
         didOpen: (toast) => {
             toast.addEventListener('mouseenter', Swal.stopTimer)
