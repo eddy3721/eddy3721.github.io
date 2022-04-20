6c9baa27eae4b509db6b24274b9adcbf89c5c9b0
eddy3721 <eddyqwq@gmail.com>
2022-04-20 16:14:59 +0800

    Update skill.js

--- js/skill.js
+++ js/skill.js
@@ -1220,7 +1220,7 @@ function sk(n, i, a, b) { //a:使用技能方 b:受技能方
             dmg = info['dmg'];
 
             msg += '<div class="flex report_blue"><div class="numberReportLine">' + i + '</div>';
-            msg += a['name'] + ' 使出了 天空俯衝!';
+            msg += a['name'] + ' 使出了 墮落魔牙!';
             if (dmg == -1) {
                 msg += ' 但是被閃開了!';
                 return obj;
